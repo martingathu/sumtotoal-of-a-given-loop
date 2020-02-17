5dadac1a08abe91c5dbd535688bf554efcf99208
martingathu <martin5gathu@gmail.com>
2020-02-17 17:08:16 +0300

    sum of numbers in a given loop

---
+++ index.js
@@ -0,0 +1,8 @@
+function mySum(n) {
+    var total=0;
+           for (let myNumber=1; myNumber<=n; myNumber++){
+               total += myNumber;
+     }
+       console.log(total)
+}
+mySum(prompt("Enter the value of n variable where n is the last number"))
